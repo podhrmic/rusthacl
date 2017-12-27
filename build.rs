8233a71cf4e5b386a9c1ce8f5744a9e070118b4c
Michal Podhradsky <mpodhradsky@galois.com>
2017-12-26 17:37:21 -0800

    Test for build script output

--- build.rs
+++ build.rs
@@ -1,19 +1,27 @@
 use std::process::Command;
+use std::env;
+
 
 fn main() {
-    Command::new("git")
+
+  let out = Command::new("git")
         .arg("clone")
         .arg("https://github.com/mitls/hacl-c.git")
         .output()
         .expect("failed to execute process");
+  println!("rusthacl git out: {:?}",out);
 
-    Command::new("make")
+  let out = Command::new("make")
         .arg("-C")
         .arg("hacl-c")
         .arg("libhacl.a")
         .output()
         .expect("failed to execute process");
+  println!("rusthacl make out: {:?}",out);
 
+  let out_dir = env::var("OUT_DIR").unwrap();
+
+  println!("rusthacl OUT_DIR = {}", out_dir);		
 	println!("cargo:rustc-link-search=hacl-c");
-    println!("cargo:rustc-link-lib=hacl");		
+  println!("cargo:rustc-link-lib=hacl");		
 }
