6b1aadf44f441d98875bec0357c986ca0e583651
Michal Podhradsky <mpodhradsky@galois.com>
2017-12-26 18:00:43 -0800

    Cleanup

--- build.rs
+++ build.rs
@@ -4,36 +4,27 @@ use std::env;
 
 fn main() {
 
-  let out = Command::new("git")
+  let _out = Command::new("git")
         .arg("clone")
         .arg("https://github.com/mitls/hacl-c.git")
         .output()
         .expect("failed to execute process");
-  println!("rusthacl git out: {:?}",out);
 
-  let out = Command::new("make")
+  let _out = Command::new("make")
         .arg("-C")
         .arg("hacl-c")
         .arg("libhacl.a")
         .output()
         .expect("failed to execute process");
-  println!("rusthacl make out: {:?}",out);
-
-  
-
-
 
   let out_dir = env::var("OUT_DIR").unwrap();
-
-  let out = Command::new("mv")
+  let _out = Command::new("mv")
         .arg("hacl-c/libhacl.a")
         .arg(out_dir + "/.")
         .output()
         .expect("failed to execute process");
-  println!("rusthacl mv out: {:?}",out);
 
-  let out_dir = env::var("OUT_DIR").unwrap();
-  println!("rusthacl OUT_DIR = {}", out_dir);		
+  let out_dir = env::var("OUT_DIR").unwrap();		
 	println!("cargo:rustc-link-search={}",out_dir);
   println!("cargo:rustc-link-lib=hacl");		
 }
