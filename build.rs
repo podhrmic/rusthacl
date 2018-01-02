9917d9d3c07415e259d6725ce4c38d97c5b262a8
Michal Podhradsky <mpodhradsky@galois.com>
2018-01-02 15:17:43 -0800

    Verbose output

--- build.rs
+++ build.rs
@@ -4,28 +4,28 @@ use std::env;
 
 fn main() {
 
-  let _out = Command::new("git")
+  let out = Command::new("git")
         .arg("clone")
         .arg("https://github.com/mitls/hacl-c.git")
         .output()
         .expect("failed to execute process");
-  println!("Rusthacl git: {:?}", _out);
+  println!("Rusthacl git: {:?}", out);
 
-  let _out = Command::new("make")
+  let out = Command::new("make")
         .arg("-C")
         .arg("hacl-c")
         .arg("libhacl.a")
         .output()
         .expect("failed to execute process");
-  println!("Rusthacl make: {:?}", _out);
+  println!("Rusthacl make: {:?}", out);
 
   let out_dir = env::var("OUT_DIR").unwrap();
-  let _out = Command::new("mv")
+  let out = Command::new("mv")
         .arg("hacl-c/libhacl.a")
         .arg(out_dir + "/.")
         .output()
         .expect("failed to execute process");
-  println!("Rusthacl mv: {:?}", _out);
+  println!("Rusthacl mv: {:?}", out);
 
   let out_dir = env::var("OUT_DIR").unwrap();		
   println!("cargo:rustc-link-search={}",out_dir);
